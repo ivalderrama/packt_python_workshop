336e6cb4cfd216e1aa67559201d1f50f849b175c
Ivan Valderrama <ivalderrama@gmail.com>
2020-06-04 20:55:15 +0100

    Added my_notes folder

---
+++ my_notes/ex35.py
@@ -0,0 +1,6 @@
+"""This is a test docstring"""
+import math
+
+def compute(numbers):
+    """This script computes the sum fo the factorial of a list of numbers"""
+    return sum([math.factorial(n) for n in numbers])
